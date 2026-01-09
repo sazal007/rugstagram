ad13e3876cce36e558e51ea0c72ad2da2086d830
Reman <mhznremon79@gmail.com>
2026-01-09 12:50:16 +0545

    ordders

--- app/(admin)/admin/orders/admin-order-page.tsx
+++ app/(admin)/admin/orders/admin-order-page.tsx
@@ -11,7 +11,7 @@ import {
   ErrorAlert,
   EmptyState,
   LoadingSpinner
-} from './components';
+} from '../../../../components/admin/orders';
 import Pagination from '@/components/ui/pagination';
 type SortDirection = 'asc' | 'desc';
 
